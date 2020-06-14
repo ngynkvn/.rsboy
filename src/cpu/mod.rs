de7af74aeb775022893bc013b76c95e45096ab3d
Kevin Nguyen <ngynkvn@gmail.com>
2020-06-14 17:57:19 -0400

    Force panic for overflow

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -156,16 +156,16 @@ impl CPU {
     fn push_stack(&mut self, value: u16, bus: &mut Bus) -> CpuResult<()> {
         println!("push {:04x} {:?}", value, INSTR_TABLE[bus.memory[value as usize] as usize]);
         let bytes = value.to_be_bytes();
-        self.set_byte(self.registers.sp.wrapping_sub(1), bytes[0], bus)?;
+        self.set_byte(self.registers.sp - 1, bytes[0], bus)?;
         self.set_byte(self.registers.sp, bytes[1], bus)?;
-        self.registers.sp = self.registers.sp.wrapping_sub(2);
+        self.registers.sp = self.registers.sp - 2;
         Ok(())
     }
 
     fn pop_stack(&mut self, bus: &mut Bus) -> CpuResult<u16> {
-        let b1 = self.read_byte(self.registers.sp.wrapping_add(1), bus);
-        let b2 = self.read_byte(self.registers.sp.wrapping_add(2), bus);
-        self.registers.sp = self.registers.sp.wrapping_add(2);
+        let b1 = self.read_byte(self.registers.sp+1, bus);
+        let b2 = self.read_byte(self.registers.sp+2, bus);
+        self.registers.sp = self.registers.sp + 2;
         let value = ((b1 as u16) << 8) | b2 as u16;
         println!("pop {:04x} {:?}", value, INSTR_TABLE[bus.memory[value as usize] as usize]);
         Ok(value)
