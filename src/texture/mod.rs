000fc8ef3b0c6f998e6b4b4545fd0f53e174878d
Kevin Nguyen <ngynkvn@gmail.com>
2020-08-01 16:10:43 -0400

    note

--- src/texture/mod.rs
+++ src/texture/mod.rs
@@ -37,18 +37,23 @@ impl Tile {
         Self { texture }
     }
 
+    // PERFORMANCE ISSUE
     pub fn write(palette: u8, pixels: &mut PixelData, location: (usize, usize), tile_data: &[u8]) {
         let (mapx, mapy) = location;
         for (y, d) in tile_data.chunks_exact(2).enumerate() {
             //Each row in tile is pair of 2 bytes.
-            for x in 0..8 {
-                let lo = d[0] >> (7 - x) & 1;
-                let hi = d[1] >> (7 - x) & 1;
-                let index = (hi << 1) | lo;
-                let color = (palette >> (index << 1)) & 0b11;
-                let c = pixel(color);
-                let location = mapx * 8 + x + mapy * 8 * 256 + y * 256;
-                pixels[location] = c;
+            if let [mut lo, mut hi] = d {
+                for x in (0..8).rev() {
+                    let lo_b = lo & 1;
+                    let hi_b = hi & 1;
+                    let index = (hi_b << 2) | lo_b << 1;
+                    let color = (palette >> index) & 0b11;
+                    let c = pixel(color);
+                    let location = mapx * 8 + x + mapy * 8 * 256 + y * 256;
+                    pixels[location] = c;
+                    lo = lo >> 1;
+                    hi = hi >> 1;
+                }
             }
         }
     }
